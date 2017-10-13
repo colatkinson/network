04ee79ffa4c29ca2547617a8ee9b5815679e29c3
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-10-13 03:38:57 -0400

    Test reducer for REP_OPEN

--- test/reducer.spec.ts
+++ test/reducer.spec.ts
@@ -3,7 +3,11 @@
 import { expect } from 'chai';
 import 'mocha';
 
+import * as zmq from 'zmq';
+
 import networkApp from '../src/reducers';
+import { repOpen } from '../src/actions';
+import * as rt from '../src/reduxTypes';
 
 describe('Redux Reducer', () => {
     it('should return the initial state', () => {
@@ -21,5 +25,32 @@ describe('Redux Reducer', () => {
         };
 
         expect(state).to.deep.equal(expected);
+    });
+
+    it('should handle REP_OPEN', () => {
+        const socket = zmq.createSocket('rep');
+
+        const state = networkApp(undefined, repOpen(socket));
+
+        socket.close();
+
+        const expected = {
+            repSock: socket,
+            reqSock: null,
+            secrets: {},
+            foreignCerts: {},
+            nativeCerts: {},
+            ipsToNames: {},
+            namesToIps: {},
+            tmpPrivKeys: {},
+        };
+
+        expect(state).to.deep.equal(expected);
+    });
+
+    it('should throw on undefined socket in REP_OPEN', () => {
+        const bound = networkApp.bind(networkApp, undefined, { type: rt.REP_OPEN, socket: undefined });
+
+        expect(bound).to.throw(Error, 'Socket undefined in REP_OPEN');
     });
 });
